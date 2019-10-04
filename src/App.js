e6ffcc65e8ba6e0a061efacc1df62450d40de069
Giant Oz <46080570+GiantOz@users.noreply.github.com>
2019-10-04 14:25:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Exporting an object containing all of our models
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+module.exports = {
+  Trip: require("./Trip"),
+  Expense: require("./Expense")
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './app.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Exporting an object containing all of our models
+
+module.exports = {
+    Trip: require("./Trip"),
+    Expense: require("./Expense")
+  };
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
